68c22401a4be348e3bd55bc2738aeb5943406f85
Robert O'Brien <brabbit111@hotmail.com>
2021-09-09 16:42:30 +1000

    Changed thousands seperator for money notification

--- server/main.lua
+++ server/main.lua
@@ -57,7 +57,7 @@ function format_thousand(v)
     local pos = string.len(s) % 3
     if pos == 0 then pos = 3 end
     return string.sub(s, 1, pos)
-            .. string.gsub(string.sub(s, pos+1), "(...)", ".%1")
+            .. string.gsub(string.sub(s, pos+1), "(...)", ",%1")
 end
 
 function GeneratePlate()
