42ffd06bb1df5be708b820884c0186a9f4972762
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-10-31 22:35:13 -0600

    Increase wait

--- server.lua
+++ server.lua
@@ -109,7 +109,7 @@ RegisterNetEvent('qb-vehicleshop:server:swapVehicle', function(data)
     local src = source
     Config.Shops[data.ClosestShop]['ShowroomVehicles'][data.ClosestVehicle].chosenVehicle = data.toVehicle
     TriggerClientEvent('qb-vehicleshop:client:swapVehicle', -1, data)
-    Wait(1000) -- let new car spawn
+    Wait(1500) -- let new car spawn
     TriggerClientEvent('qb-vehicleshop:client:homeMenu', src) -- reopen main menu
 end)
 
