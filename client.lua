1a116c4bba9ab58ae396696d44615a1f6d75e241
Evantually <egbeesefl@gmail.com>
2022-02-09 13:11:52 -0600

    transfervehicle command fix

--- client.lua
+++ client.lua
@@ -363,6 +363,18 @@ end
 
 -- Events
 
+RegisterNetEvent('qb-vehicleshop:client:transferVehicle', function(buyerId, amount)
+    local ped = PlayerPedId()
+    local vehicle = GetVehiclePedIsIn(ped, false)
+    local plate = QBCore.Functions.GetPlate(vehicle)
+    local tcoords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(buyerId)))
+    if #(GetEntityCoords(ped)-tcoords) < 5.0 then
+        TriggerServerEvent('qb-vehicleshop:server:transferVehicle', plate, buyerId, amount)
+    else
+        QBCore.Functions.Notify('The person you are selling to is too far away.')
+    end
+end)
+
 RegisterNetEvent('qb-vehicleshop:client:homeMenu', function()
     exports['qb-menu']:openMenu(vehicleMenu)
 end)
